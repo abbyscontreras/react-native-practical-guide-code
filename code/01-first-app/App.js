8d7afc4b01b03800b46cbafea882cfbe7ee01f69
abbyscontreras <abigailscontreras@gmail.com>
2024-10-13 21:29:42 -0400

    improving the layout

--- code/01-first-app/App.js
+++ code/01-first-app/App.js
@@ -12,7 +12,7 @@ export default function App() {
         />
         <Button title='Add Goal' />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -21,19 +21,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     // Set padding to 75 for top and bottom, 50 for left and right
-    paddingVertical: 75,
-    paddingHorizontal: 50,
+    paddingTop: 75,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8
   },
+  goalsContainer: {
+    flex: 5
+  }
 });
